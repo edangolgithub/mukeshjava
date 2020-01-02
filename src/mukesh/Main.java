43bea5b0c57bc53fb2da3cd81d04df707c083fa1
evan dangol <dangolevan@hotmail.com>
2020-01-02 10:42:30 +0545

    43

---
+++ src/mukesh/Main.java
@@ -0,0 +1,64 @@
+package mukesh;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		
+
+		
+		
+	
+		//arithematic op
+		
+		int x=5;
+		int y=2;
+		int z=x%y;
+		//System.out.println(z);
+		
+		
+		//increment/decrement operator ++ --  post pre (fix)  a++ ++a a-- --a
+		
+		
+		int xx=10;
+		xx++; //xx=xx+1;
+		
+		//System.out.println(xx++); 
+		//System.out.println(xx);
+		
+		
+		//Assignment op  =  += -= *= /= %=
+		
+		int a=10;
+		a%=20; // a=a+20;
+		//System.out.println(a);
+		
+		
+		
+		//relational op == < > <= >= !=
+		
+		//System.out.println(5!=5);
+		
+		
+		//Logical op  and or not   &&  ||  !  
+		
+		
+		//System.out.println(3>6 || 5<1 ); //false and true => false
+		
+		
+		//System.out.println(!(3>7));
+		
+		// Bit wise op  & | ~ ^
+		//2=> 10
+		//3=> 11
+		
+		//   11
+		//^  10
+		//-------
+		//   01
+		
+		System.out.println(2^3);
+		
+	}
+
+}
